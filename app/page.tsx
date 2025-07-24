2a28879873f6271e86899b0de32e8e2f56767b2a
cbeAbishek <agsabhishek1907@gmail.com>
2025-07-25 00:27:06 +0530

    update analysis

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Hero from "@/components/home/hero"
 import Benefits from "@/components/home/benefits"
 import FeaturedProducts from "@/components/home/featured-products"
@@ -7,8 +8,21 @@ import CallToAction from "@/components/home/call-to-action"
 import OfferPopupCard from "@/components/Popup/popup"
 import { AiChatbot } from "@/components/chatbot";
 import FMCGPreloader from "@/components/preloader";
+import { useEffect } from "react";
+
 
 export default function Home() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.defer = true;
+    script.src = "https://cloud.umami.is/script.js";
+    script.setAttribute("data-website-id", "e7e5b06b-5440-4664-8b38-cbcbb40c1ab9");
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
   return (
     <div className="flex flex-col gap-16">
       <FMCGPreloader />
@@ -19,6 +33,7 @@ export default function Home() {
       <FeaturedProducts />
       <Testimonials />
       <CallToAction />
+      
     </div>
   )
 }
