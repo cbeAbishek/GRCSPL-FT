07809065593e0e6543d211ed7f765017d9b610a1
cbeAbishek <agsabhishek1907@gmail.com>
2025-07-25 19:31:44 +0530

    update analysis for product page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,7 +1,7 @@
+"use client";
 import type { Metadata } from "next";
 import ProductGrid from "@/components/ProductGrid";
-import { Helmet } from "react-helmet-async";
-import { products } from "@/lib/prop";
+import { useEffect } from "react";
 
 export const metadata: Metadata = {
   title: "Products | Business Opportunities in Tamil Nadu",
@@ -10,10 +10,19 @@ export const metadata: Metadata = {
 
 
 export default function ProductsPage() {
- 
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.defer = true;
+    script.src = "https://cloud.umami.is/script.js";
+    script.setAttribute("data-website-id", "6576a669-1fa5-4845-ab29-70071ad0da8b");
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
   return (
     <div>
-
      <ProductGrid />
     </div>
   );
